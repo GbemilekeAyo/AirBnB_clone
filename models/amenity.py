c58274ae82057a788e957c984b857dae8a769cf7
Gbemileke Ayodele <105323737+GbemilekeAyo@users.noreply.github.com>
2024-11-17 17:58:31 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""Module for Amenity class."""
-
+"""
+class Amenity that inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Class representing a Amenity."""
+    """
+        Summary: Definning the Amenity class that inherits from BaseModel
+        Public class attributes:
+            name string - empty string
+    """
     name = ""
