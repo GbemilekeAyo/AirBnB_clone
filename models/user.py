a3d509e882579d72259040b57c586d844acc6e0c
Gbemileke Ayodele <105323737+GbemilekeAyo@users.noreply.github.com>
2024-11-17 18:07:23 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""Module for User class."""
+"""
+class User that inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class representing a User."""
+    """
+        Summary: Definning the User class that inherits from BaseModel
+        Public class attributes:
+            email: string - empty string
+            password: string - empty string
+            first_name: string - empty string
+            last_name: string - empty string
+    """
     email = ""
     password = ""
     first_name = ""
